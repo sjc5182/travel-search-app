322ce4da037e0bee25c36bb68a0341e6d87d7e36
sjc5182 <shiguangchen1987@gmail.com>
2019-02-08 22:55:48 -0800

    first flight search app commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-class App extends Component {
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+
+  componentDidMount(){
+
+    var config = {
+      headers: {'X-RapidAPI-Key': '9582c9a04bmsh38417e4edecff7dp13f60djsn521655bbeed3'}
+    };
+    
+    axios.get('https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-02-20?inboundpartialdate=2019-01-01', config)
+  .then(function (result) {
+    console.log(result.data.Carriers);
+  });
+
+
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <p>Hello</p>
       </div>
     );
   }
