0f0a727efcf1eaa4828ed6265b9394be285e95a1
sjc5182 <shiguangchen1987@gmail.com>
2019-02-10 23:36:09 -0800

    update

--- src/App.js
+++ src/App.js
@@ -26,30 +26,67 @@ class App extends React.Component {
 
   handleSetApi = () =>{
     const {country, currency, language, departure, arrival, departureDate } = this.state
+    console.time("hello")
     const url = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/${country}/${currency}/${language}/${departure}/${arrival}/${departureDate}`
+    console.timeEnd("hello")
     const config = {
       headers: {'X-RapidAPI-Key': '9582c9a04bmsh38417e4edecff7dp13f60djsn521655bbeed3'}
     };
     axios.get(url, config)
       .then((result) => {
+        console.log(result.data);
+        const Test = result.data.Carriers.concat(result.data.Quotes)
         this.setState({
-          carriers: result.data.Carriers,
-          quotes: result.data.Quotes
+          carriers: Test
         })
       });
   }
   
+  componentDidMount(){
+  const config = {
+    headers: {'X-RapidAPI-Key': '9582c9a04bmsh38417e4edecff7dp13f60djsn521655bbeed3'}
+  };
+  axios.get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/8b8cf6c0-7a86-4c91-b1cf-b185efd0e028", config)
+    .then((result) => {
+      // console.log(result.data);
+      // const Test = result.data.Carriers.concat(result.data.Quotes)
+      
+      let test = []
+      let itineraries = result.data.Itineraries;
+      let legs = result.data.Legs;
+      // legs.map((leg) => { 
+      //   result = itineraries.filter(itinerary => itinerary.OutboundLegId===leg.Id);
+     
+      let data = Object.assign({}, itineraries.OutboundLegId, legs)
+        
+        
+      //   return 
+      // })
+      // legs.map((leg) => {
+      //   let data = Object.assign({}, leg)
+      //   return test.push(data)
+      // })
+      
+      
+      
+      
+      
+     console.log(data)
+    });
+  }
+
   render() {
     return (
       <div>
-        <input type = "text" name = "country" onChange = {this.handleSearch} placeholder = "US"></input>
+        {/* <input type = "text" name = "country" onChange = {this.handleSearch} placeholder = "US"></input>
         <input type = "text" name = "currency" onChange = {this.handleSearch} placeholder = "USD"></input>
         <input type = "text" name = "language" onChange = {this.handleSearch} placeholder = "en-US"></input>
         <input type = "text" name = "departure" onChange = {this.handleSearch} placeholder = "SFO-sky"></input>
         <input type = "text" name = "arrival" onChange = {this.handleSearch} placeholder = "JFK-sky"></input>
         <input type = "text" name = "departureDate" onChange = {this.handleSearch} placeholder = "2019-02-20"></input>
         <button onClick = {this.handleSetApi}>Search</button>
-        <SearchResult carriers = {this.state.carriers} quotes = {this.state.quotes}/>
+        <SearchResult carriers = {this.state.carriers} /> */}
+        {/* <SearchResult carriers = {this.state.carriers} /> */}
       </div>
     );
   }
--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -3,9 +3,12 @@ import React from 'react';
 const SearchResult = (props) => (
   //const {carriers, quotes} = props
   <div>
-    {props.carriers.map(carrier => <div key = {carrier.CarrierId}>{carrier.Name}</div>)}
+    {/* {props.carriers.map(carrier => <div>{carrier.MinPrice}</div>)} */}
   </div>
 );
 
 
 export default SearchResult;
+
+
+// {props.carriers.map(carrier => <div key = {carrier.CarrierId}>{carrier.Name}</div>)}
